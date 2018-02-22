aace6f838ce0b09ddbb8f7fab4d942252e88004e
St0gas <4achat@mail.com>
2018-02-22 13:53:56 +0200

    v1.01 Updated HM

--- 8paskaita/script.js
+++ 8paskaita/script.js
@@ -1,3 +1,28 @@
+// // querySelectorAll leidzia istraukti visus li elementus i masyva, su kurio darysim manipuliacijas.
+
+
+// var list = document.querySelectorAll ('li');
+
+// // ----------------------------------------------------------------------------
+
+// // console.log (list);   Galim pasitikrinti ar sukure array is visu li elementu
+
+// // Naudojam FOR isselektinti kiekviena li elementa is array
+// for (i = 0 ; i < list.length; i++) {					
+
+
+// 		// Savikontrolei spausdinam kiekviena li elementa su indeksu (vieta kuria uzima masyve)
+// 		console.log(list[i]);
+
+// 		// kiekvienam li elementui sukuriam monitoringa klikui.
+// 		list[i].addEventListener('click',function(e){
+
+// 		// paspaudus li elementa galim pridet/panaikint klase, nors sioje vietoje logiskiau naudot [add] metoda. Nes dingus li elementui nera kur spausti kad jis atsirastu vel.
+// 		e.currentTarget.classList.toggle('hide');
+
+// })};
+
+
 // querySelectorAll leidzia istraukti visus li elementus i masyva, su kurio darysim manipuliacijas.
 
 
@@ -15,10 +40,17 @@ for (i = 0 ; i < list.length; i++) {
 		console.log(list[i]);
 
 		// kiekvienam li elementui sukuriam monitoringa klikui.
-		list[i].addEventListener('click',function(e){
+		list[i].addEventListener('mouseenter',function(e){
 
 		// paspaudus li elementa galim pridet/panaikint klase, nors sioje vietoje logiskiau naudot [add] metoda. Nes dingus li elementui nera kur spausti kad jis atsirastu vel.
-		e.currentTarget.classList.toggle('hide');
+		e.currentTarget.classList.add('background');
 
-})};
+})
+			list[i].addEventListener('mouseleave',function(e){
+
+		// paspaudus li elementa galim pridet/panaikint klase, nors sioje vietoje logiskiau naudot [add] metoda. Nes dingus li elementui nera kur spausti kad jis atsirastu vel.
+		e.currentTarget.classList.remove('background');
+
+	})};
+
 
