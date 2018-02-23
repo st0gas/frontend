8278e45392621813f623832c62cab0b1374b02cf
St0gas <4achat@mail.com>
2018-02-23 03:23:44 +0200

    Prideti komentarai

--- 10paskaita_praktika/bootstrap-examples.js
+++ 10paskaita_praktika/bootstrap-examples.js
@@ -1,13 +1,19 @@
 
 $(function() {
     $('#process').on('click', function(event) {
-             $(".progress-bar").animate({
+            // Animate reikalauja pilnos jquery bibliotekos: https://code.jquery.com/jquery-3.3.1.min.js
+             $(".progress-bar").animate({   
             width: "100%"
         }, {
+        //Naustatom kiek laiko animacija tesis
         duration:1000,
+        // Nustatom kokius veiksmus atlikti po animacijos
         complete: function() {
+        // Keičiam spalvą button
         $(event.target).removeClass('btn-dark').addClass('btn-success');
+        // Nebūtina bet fiksuojam esamą ilgį
         $(".progress-bar").attr('aria-valuenow','100');
+        // Nuimam stebėjimą nuo mygtuko kad nesikartotu animacija paspaudus
         $(event.target).unbind('click');
     }});
 })});
